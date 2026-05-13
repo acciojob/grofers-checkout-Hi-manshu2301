579d12024477c50a1a2ad1767c7257ce7cb6716d
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-05-13 16:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+	let total =0;
+	prices.forEach(function(price){
+		total += Number(price.textContent);
+	});
+	const row = document.createElement("tr");
+	const cell = document.createElemtnt("td");
+	cell.colSpan = 2;
+	cell.textContent = total;
+	row.appenChild(cell);
+	document.querySelector("table").appendChild(row);
   
 };
+getSumBtn.addEventListener("click",getSum);
 
 getSumBtn.addEventListener("click", getSum);
 
